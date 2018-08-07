ea43dbe68fd4fefcb2b6f4fc1c7da6011304ff3b
AuraMelisa01 <auramelisa6@gmail.com>
2018-08-06 23:46:40 -0500

    agregando pequeño json

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,16 @@
-window.busqueda = (restaurantes) => {
 
-}
+const btnBuscar = document.getElementById('btnBuscar');
+const txtComida = document.getElementById('txtComida').value;
+mexicana.style.display = 'none';
+vegetariana.style.display = 'none';
+
+btnBuscar.addEventListener('click', e => {
+  if (txtComida == "mexicana"){
+     mexicana.style.display = 'block';
+  } else if (txtComida == "vegetariana"){
+     vegetariana.style.display = 'block';
+  }else {
+    alert("No se encuentra tu busqueda");
+  }
+
+});
--- src/js/data.js
+++ src/js/data.js
@@ -1,14 +1,1 @@
-const json = '../data/restaurantes.json';
 
-window.realoadJSON = () => {
-  fetch(json)
-    .then(response => response.json())
-    .then((res) => {
-      const infoStudent = computeStudentsStats(res);
-      const infoGeneration = computeGenerationsStats(res);
-      filterStudents(infoStudent);
-      studentDraw(infoStudent);
-      generationDraw(infoGeneration);
-    })
-    .catch(error => {console.log(error)});
-};
